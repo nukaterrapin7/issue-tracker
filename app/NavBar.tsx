1ac2731e334997ca3f566cb094312e2a9f8a9783
scott <pythonism007@gmail.com>
2024-12-05 09:08:13 -0600

    Styles the active link in the nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+'use client';
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { PiBugBold } from 'react-icons/pi'
+import classnames from 'classnames';
 
 const NavBar = () => {
+    const currentPath = usePathname();
+
     const links = [
         { label: 'Dashboard', href: '/'},
         { label: 'Issues', href: '/issues'}
@@ -13,7 +19,12 @@ const NavBar = () => {
       <ul className='flex space-x-6'>
         {links.map(link => 
             <Link 
-                key={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+                key={link.href} 
+                className={classnames({
+                    'text-zinc-900': link.href === currentPath,
+                    'text-zinc-500': link.href !== currentPath,
+                    'hover:text-zinc-800 transition-colors': true
+                })} 
                 href={link.href}>{link.label}</Link>)}
       </ul>
     </nav>
