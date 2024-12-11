5fba95e958191f1b589640be2840c68ff7eb4281
scott <pythonism007@gmail.com>
2024-12-11 15:49:09 -0600

    adds a spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -12,6 +12,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
@@ -20,7 +21,9 @@ const NewIssuePage = () => {
   const {register, control, handleSubmit, formState: { errors }} =  useForm<IssueForm>({
     resolver: zodResolver(createIssueSchema)
   });
+
   const [error, setError] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   return (
     <div className='max-w-xl'>
@@ -29,9 +32,11 @@ const NewIssuePage = () => {
         </Callout.Root>}
       <form className=' space-y-3' onSubmit={handleSubmit(async (data) => {
         try {
+          setIsSubmitting(true);
           await axios.post('/api/issues', data);
           router.push('/issues')  
         } catch (error) {
+          setIsSubmitting(false);
           setError('An unexpected error has occured.')
         }
       })}>
@@ -47,7 +52,9 @@ const NewIssuePage = () => {
         <ErrorMessage>
           {errors.description?.message}
         </ErrorMessage>
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   )
